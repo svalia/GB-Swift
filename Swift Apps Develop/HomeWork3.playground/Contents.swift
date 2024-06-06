8ab8ddc05f898951f0e90ef1c59ec5011e99a64c
Valia Smolin <Valia@Smolin.Ru>
2024-06-07 02:04:48 +0300

    Done Homework4

--- Swift Apps Develop/HomeWork3.playground/Contents.swift
+++ Swift Apps Develop/HomeWork3.playground/Contents.swift
@@ -85,3 +85,101 @@ func optionalArray (a: [Int?]) -> Int {
     return b
 }
 optionalArray(a: [2, 5, nil, 10, nil, -12, 50, 40, nil])
+
+
+//Создать массив, состоящий из целых чисел от 3 до 10, не включая 10, вывести для каждого элемента сам элемент и его индекс.
+var arr2 = Array(3..<10)
+
+for (index, i) in arr2.enumerated() {
+    print(i)
+    print(index)
+}
+
+//Создать пустой словарь. Добавить в него 5 элементов, начиная с 0, где ключ - текущий номер элемента в виде строки, а значение - элемент больше или равен трем (то есть или true, или false). Добавлять элементы нужно при помощи цикла. То есть у Вас в итоге должно получиться примерно следующее, естественно, порядок может быть другой: ["0": false, "1": false, "3": true, "4": true, "2": false]
+var dict2: [String: Bool] = [:]
+
+for i in 0...4 {
+    dict2.updateValue(i >= 3, forKey: String(i))
+}
+
+//Написать функцию, которая на вход принимает массив строк. Если в массиве находится строка, длиной менее 5 символов, то вернуть nil. В противном случае вернуть самую маленькую по длине строку. Если несколько - вернуть любую из них
+
+func arrayString (a: [String]) -> String? {
+    guard !a.isEmpty else {
+        return nil
+    }
+    var minString = a.first ?? ""
+    for i in a {
+        if i.count < 5 {
+            return nil
+        }
+        else {
+            if i.count < minString.count {
+                minString = i
+            }
+        }
+    }
+    return minString
+}
+//Написать функцию, которая на вход принимает массив целых чисел. В функции, пока количество элементов массива больше 5, удалять первый элемент. Функция должна вернуть новый получившийся массив. Если количество элементов в массиве изначально меньше или равно 5 - вернуть nil.
+
+func arrayInt (a: [Int]) -> [Int]? {
+    guard a.count > 5 else {
+        return nil
+    }
+    var b = a
+    while b.count > 5 {
+        b.removeLast()
+    }
+    return b
+}
+
+func my (a: [Int], b: Int) -> [Int] {
+    guard a.count >= b else {
+        return []
+    }
+    var c = a
+    c.removeFirst(b)
+    return c
+}
+
+func my2 (a: [String]) -> [String] {
+    guard !a.isEmpty else {
+        return []
+    }
+    var b: [String] = []
+    for i in a {
+        if !i.isEmpty {
+            b.append(i)
+        }
+    }
+    return b
+}
+
+func my3 (_ a: [String?]?) -> [String] {
+    guard let a else {
+        return []
+    }
+    var b: [String] = []
+    for i in a {
+        if let i {
+            b.append(i)
+        }
+    }
+    return b
+}
+
+func my4 (label a: [String?]?) -> [Int] {
+    guard let a else {
+        return []
+    }
+    var b: [Int] = []
+    for currentElement in a {
+        if let currentElement, let c = Int(currentElement) {
+            b.append(c)
+        }
+    }
+    return b
+}
+
+
