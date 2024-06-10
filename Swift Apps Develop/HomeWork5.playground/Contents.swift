9e2169dad4444198ed2f8fab79ace1c3b76dcf98
Valia Smolin <Valia@Smolin.Ru>
2024-06-10 23:25:32 +0300

    Done Cake struct&enum5

--- Swift Apps Develop/HomeWork5.playground/Contents.swift
+++ Swift Apps Develop/HomeWork5.playground/Contents.swift
@@ -1,26 +1,51 @@
 import UIKit
-
+//--------------Задание 1-----------
 //1. Создать структуру пирожного. У пирожного должно быть название, стоимость и вкус (например, клубничное, малиновое, черничное). Название должно быть вычисляемым свойством и возвращать вкус + “cake”, например, “strawberry cake”. Вкус должен быть уровня internal, а стоимость private. Написать инициализатор, в котором будет устанавливаться вкус и стоимость.
 //2. Написать в структуре пирожного функцию, которая позволяет получить стоимость.
 //3& Написать в структуре пирожного функцию, которая позволяет изменить стоимость, исходя из полученной скидки. Если полученная скидка меньше или равна 30%, то установить новую стоимость за вычетом скидки, в противном случае не изменять стоимость и вывести сообщение, что скидка должна быть меньше или равна 30%.
+
+//--------------Задание 2-----------
+//1. Сделать вкус в структуре пирожного перечислением.
+//2. Добавить возможность украсить пирожное. Для этого нужно создать перечисление с возможными видами украшения. Добавить в пирожное свойство, в котором будет хранится вариант украшения. Не забыть проинициализировать его.
+//3. Сделать так, чтобы можно было использовать сразу несколько украшений.
+
+enum Taste: String {
+    case lemon
+    case strawberry
+    case chokolate
+}
+
+enum CakeDecoration {
+    case Cream
+    case fruitSlices
+    case icing
+    case nuts
+    case powderedSugar
+    
+}
+
 struct Cake {
     private var cost: Double
-    internal var taste: String
+    var taste: Taste
+    var decoration: [CakeDecoration]
     var name: String {
         get {
-            taste + " Cake"
+            taste.rawValue + " Cake"
         }
         set (newTaste) {
             "Cake" + newTaste
         }
     }
     
-    init(cost: Double, taste: String) {
+    init?(cost: Double, taste: Taste, decoration: [CakeDecoration]?) {
         self.cost = cost
         self.taste = taste
-        
+        guard let decoration else {
+            return nil
+        }
+        self.decoration = decoration
     }
-    mutating func getCost () -> Double {
+    func getCost () -> Double {
         return cost
     }
     mutating func setCost (discount:Int) -> Double {
@@ -36,6 +61,7 @@ struct Cake {
     }
 }
 
-var lemonCake = Cake(cost: 10.5, taste: "Lemon")
-print(lemonCake.name)
-lemonCake.getCost()
+var lemonCake = Cake(cost: 10.5, taste: .chokolate, decoration: [.Cream, .nuts])
+print(lemonCake?.name)// после добавления опционалного инициализатора, xcode попросил тут добавить ? - примерно понял почему, но хочу чтобы понять наверняка
+lemonCake?.getCost() //тут тоже самое
+
--- Swift Apps Develop/structure.playground/Contents.swift
+++ Swift Apps Develop/structure.playground/Contents.swift
@@ -3,33 +3,34 @@ import UIKit
 struct Lemonade {
     var name: String
     private var cost: Double
-    var size: String
-    var promoCode: String//1. Добавить в структуру лимонада промокоды. Это будет строка (свойство с типом строка)
+    var size: Size
+    
+    var promoCode: PromoCode//1. Добавить в структуру лимонада промокоды. Это будет строка (свойство с типом строка)
 
     
     var sirope: String {
         get {
-            name + size
+            name + size.rawValue
         }
         set (newSirope) {
             name = newSirope + name
         }
     }
-    init(name: String = "Cola", cost: Double, size: String, promoCode: String) {
+    init(name: String = "Cola", cost: Double, size: Size, promoCode: PromoCode) {
         self.name = name
         self.cost = cost
         self.size = size
         self.promoCode = promoCode
     }
-    init?(basename: String = "Fanta", cost: Double, size: String, promoCode: String) {
+    init?(basename: String = "Fanta", cost: Double, size: Size, promoCode: PromoCode) {
         name = basename
-        if size == "S" {
+        if size == .s {
             self.cost = cost
         }
-        else if size == "M" {
+        else if size == .m {
             self.cost = cost * 0.9
         }
-        else if size == "L" {
+        else if size == .l {
             self.cost = cost * 0.85
         }
         else {
@@ -46,16 +47,19 @@ struct Lemonade {
         cost = newCost
     }
 //    2. Написать функцию в структуре лимонада, которая на основе промокода устанавливает новую стоимость для лимонада. Если промокод “happy”, то стоимость уменьшается на 10%, если “new”, то на 15%, а если “year”, то на 20%. Функция ничего не возвращает
-    mutating func setPromoCost (promoCode: String) {
-        var x: Double
-        switch promoCode {
-        case "happy" : x = 0.9
-        case "new" : x = 0.85
-        case "year" : x = 0.80
-        default : x = 1
+    mutating func setPromoCost (promoCode: PromoCode?) {
+        
+        guard let promoCode else {
+        return
         }
-        cost = cost * x
+        switch promoCode {
+        case .happy(let discount): cost *= discount
+        case .new(let discount): cost *= discount
+        case .year(let discount): cost *= discount
+        case .newSize(let discount, let promoSize): cost *= discount
+        }
     }
+        
 
 }
 
@@ -66,6 +70,58 @@ struct Lemonade {
 //print(cola.sirope)
 //cola.sirope = "orange"
 //print(cola.name)
-var cola3 = Lemonade(basename: "Sprite", cost: 0.5, size: "M", promoCode: "1a2Sasd")
-print(cola3?.returnCost() ?? 10.0)
+//var cola3 = Lemonade(basename: "Sprite", cost: 0.5, size: .m, promoCode: "1a2Sasd")
+//print(cola3?.returnCost() ?? 10.0)
 
+enum Size: String {
+    case s
+    case m
+    case l
+    static var size = "size"
+    func getNameSize (name:String) -> String {
+        name + self.rawValue + Size.size
+    }
+    //Написать функцию в перечислении Size, которая на основе промокода и размера возвращает можно ли увеличить размер бесплатно. Если промокод “newSize” и размер не l, то вернуть true, в противном случае false.
+
+    mutating func getNewSize (promoCode: PromoCode) -> Bool {
+        switch promoCode {
+        case .newSize(_ , let size) where self != .l:
+            self = size
+            return true
+        default: false
+        }
+        return false
+    }
+    init? (cost: Double) {
+        guard cost >= 0 else {
+            return nil
+        }
+        switch cost {
+        case 0..<100 : self = .s
+        case 100..<300 : self = .m
+        case 300... : self = .l
+        default : return nil
+        }
+        
+    }
+}
+print(Size.size)
+
+enum PromoCode {
+    case happy (Double)
+    case new (Double)
+    case year (Double)
+    case newSize (Double, Size)
+}
+var s = Size.s
+var m: Size = .m
+var l = Size(rawValue: "large")
+
+s.getNameSize(name: "Lemon ")
+m.getNameSize(name: "lemon ")
+
+var lemonad = Lemonade(name: "Cola", cost: 10.5, size: .l, promoCode: .happy(10.0))
+lemonad.setPromoCost(promoCode: .happy(10.0))
+lemonad.setPromoCost(promoCode: .newSize(10, .l))
+lemonad.setPromoCost(promoCode: .new(20.0))
+lemonad.setPromoCost(promoCode: .year(30.0))
